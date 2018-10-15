d11cef2308d215d393bbec75bf036d6153f0fe1e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-15 21:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const dodger = document.getElementById('dodger')
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
